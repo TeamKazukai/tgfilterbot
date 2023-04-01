e7acabcdbbd4f108e8749e7b01fe02441d5051af
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-04-02 01:41:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -45,7 +45,7 @@ SPELL_CHECK = {}
         #await auto_filter(client, message)
 
 
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
