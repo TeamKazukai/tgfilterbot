ca69726441aaeb978cdeb96c19316403afc5aa7b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-04-01 21:33:21 +0530

    Update rules.py

--- plugins/EXTRA/rules.py
+++ plugins/EXTRA/rules.py
@@ -110,9 +110,9 @@ async def r_message(client, message):
 
 
 
-@Client.on_message(filters.private & filters.command("new") & filters.user(ADMINS))             
+@Client.on_message(filters.private & filters.reply & filters.user(ADMINS))             
 async def start_message(client, message):
-    reply = message.reply_to_message
+#    reply = message.reply_to_message
 # @Client.on_message(filters.private & filters.forwarded)
 # async def start_message(client, message):
     searchh = message.text                 
