4cb00dfa3f6614a1685223868b717ce7952b55a0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-04-01 22:38:47 +0530

    Update rules.py

--- plugins/EXTRA/rules.py
+++ plugins/EXTRA/rules.py
@@ -117,7 +117,8 @@ async def start_message(client, message):
 #    reply = message.reply_to_message
 # @Client.on_message(filters.private & filters.forwarded)
 # async def start_message(client, message):
-    searchh = message.text                 
+    searchh = message.text 
+    files, n_offset, total = await get_search_results(query.message.chat.id, search, offset=offset, filter=True)            
     imdb = await get_poster(searchh) if IMDB else None 
     if imdb:
 
