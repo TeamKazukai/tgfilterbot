637d64b7394ded738f7362a29fb02d3d2614fcd3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-04-02 00:34:48 +0530

    Update rules.py

--- plugins/EXTRA/rules.py
+++ plugins/EXTRA/rules.py
@@ -160,7 +160,7 @@ async def start_message(client, message):
             buttons = [[
                 InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐆𝐫𝐨𝐮𝐩', url=f'http://t.me/nasrani_update'),
                 InlineKeyboardButton("𝐒𝐮𝐫𝐩𝐫𝐢𝐬𝐞", url=f"https://telegram.me/{temp.U_NAME}?start"),
-                InlineKeyboardButton('𝐋𝐞𝐭𝐞𝐬𝐭 𝐓𝐫𝐲', url=f'BATCH_LINK')      
+                InlineKeyboardButton('𝐋𝐞𝐭𝐞𝐬𝐭 𝐓𝐫𝐲', url=f'(BATCH_LINK)')      
             ]]
             reply_markup = InlineKeyboardMarkup(buttons)
             await message.reply_photo(photo=imdb.get('poster'), caption=cap,
@@ -174,7 +174,7 @@ async def start_message(client, message):
             buttons = [[
                 InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐆𝐫𝐨𝐮𝐩', url=f'http://t.me/nasrani_update'),
                 InlineKeyboardButton("𝐒𝐮𝐫𝐩𝐫𝐢𝐬𝐞", url=https://telegram.me/{temp.U_NAME}?start"),
-                InlineKeyboardButton('𝐋𝐞𝐭𝐞𝐬𝐭 𝐓𝐫𝐲', url=f'BATCH_LINK')           
+                InlineKeyboardButton('𝐋𝐞𝐭𝐞𝐬𝐭 𝐓𝐫𝐲', url=f'(BATCH_LINK)')           
             ]]
             hmm = await message.reply_photo(photo=poster, caption=cap,
             reply_markup=reply_markup,
