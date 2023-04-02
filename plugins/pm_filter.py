483e11d0b707ed8b64c1130520e4c98feb65a1bf
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-04-02 16:53:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2345,7 +2345,7 @@ async def advantage_spell_chok(client, msg):
 
 
 
-async def manual_filters(client, message, text=False):
+async def manual_filters(client, message, text=True):
     settings = await get_settings(message.chat.id)
     group_id = message.chat.id
     name = text or message.text
@@ -2487,7 +2487,7 @@ async def manual_filters(client, message, text=False):
 
 
 
-async def global_filters(client, message, text=False):
+async def global_filters(client, message, text=True):
     settings = await get_settings(message.chat.id)
     group_id = message.chat.id
     name = text or message.text
