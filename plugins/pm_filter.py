14beec7dfdc7f5e8a1e2f1bc243d0f7196cacf0d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-04-02 20:50:09 +0530

    Update and rename pm_filter.exe to pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,7 +40,80 @@ SPELL_CHECK = {}
 FILTER_MODE = {}
 
 
-                    
+@Client.on_message(filters.group & filters.text & filters.incoming)
+async def give_filter(client, message):
+    content = message.text
+    settings = await get_settings(message.chat.id)        
+    if settings["auto_ffilter"]:
+        userid = message.from_user.id if message.from_user else None
+        if not userid:
+            search = message.text
+            k = await message.reply(f"You'r anonymous admin! Sorry you can't get '{search}' from here.\nYou can get '{search}' from bot inline search.")
+            await asyncio.sleep(30)
+            await k.delete()
+            try:
+                await message.delete()
+            except:
+                pass
+            return
+
+        if LOGIN_CHANNEL and not await mute_login(client, message):
+            try:
+                invite_link = await client.create_chat_invite_link(int(LOGIN_CHANNEL))
+            except ChatAdminRequired:
+                logger.error("Make sure Bot is admin in Forcesub channel")
+                return
+            buttons = [[
+                InlineKeyboardButton("📢 𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 📢", url=invite_link.invite_link)
+            ],[
+                InlineKeyboardButton("🔁 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧 🔁", callback_data="grp_checksub")
+            ]]
+            reply_markup = InlineKeyboardMarkup(buttons)
+#            try:
+#                await client.restrict_chat_member(message.chat.id, message.from_user.id, ChatPermissions(), datetime.now() + timedelta(minutes=1))
+#            except:
+#                pass
+            k = await message.reply_photo(
+                photo=random.choice(SP),
+                caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention},\n\n{content} 𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
+                reply_markup=reply_markup,
+                parse_mode=enums.ParseMode.HTML
+            )
+            await asyncio.sleep(300)
+            await k.delete()
+            try:
+                await message.delete()
+            except:
+                pass
+        else:
+            await auto_filter(client, message)
+    else:
+        k = await message.reply_text(f"𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention},\n\n{content} 𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞..!! \n\n❌️𝐀𝐮𝐭𝐨 𝐅𝐢𝐥𝐭𝐞𝐫 𝐎𝐟𝐟..!!!❌️ \n𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭..")
+        await asyncio.sleep(5)
+        await k.delete()
+        try:
+            await message.delete()
+        except:
+            pass
+            return
+        if message.chat.id != SUPPORT_CHAT_ID:
+            await global_filters(client, message)
+        manual = await manual_filters(client, message)
+        if manual == False:
+            settings = await get_settings(message.chat.id)
+            try:
+                if settings['auto_ffilter']:
+                    await auto_filter(client, message)
+            except KeyError:
+                grpid = await active_connection(str(message.from_user.id))
+                await save_group_settings(grpid, 'auto_ffilter', False)
+                settings = await get_settings(message.chat.id)
+                if settings['auto_ffilter']:
+                    await auto_filter(client, message) 
+                    try:
+                        await message.delete()
+                    except:
+                        pass                    
 
 
 
@@ -2270,7 +2343,7 @@ async def advantage_spell_chok(client, msg):
                 await spell_check_del.delete()
 
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
+async def manual_filters(client, message, text=False):
 async def give_filter(client,message):
 # async def manual_filters(client, message, text=False):
     settings = await get_settings(message.chat.id)
@@ -2403,8 +2476,7 @@ async def give_filter(client,message):
                     logger.exception(e)
                 break
     else:
-        await auto_filter(client, message)
-
+        return False
 
 
 
@@ -2413,9 +2485,8 @@ async def give_filter(client,message):
                                
 
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
-async def give_filter(client,message):
-# async def global_filters(client, message, text=False):
+
+async def global_filters(client, message, text=False):
     settings = await get_settings(message.chat.id)
     group_id = message.chat.id
     name = message.text
@@ -2471,4 +2542,4 @@ async def give_filter(client,message):
                     logger.exception(e)
                 break
     else:
-        await auto_filter(client, message)
+        return False
